728401c253a373e22a57edd11e7eeac188a3ede2
kwonup <kkwon1122@gmail.com>
2024-06-11 18:34:21 +0900

    feat: image-list 링크 연결

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const apiUrl = "https://collectionapi.metmuseum.org/public/collection/v1/departments"; // 여기에 실제 API URL을 입력하세요.
+    const apiUrl = "https://collectionapi.metmuseum.org/public/collection/v1/departments";
     const departmentList = document.getElementById("department-list");
 
     fetch(apiUrl)
@@ -9,8 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 const listItem = document.createElement("li");
                 listItem.textContent = department.displayName;
                 listItem.dataset.id = department.departmentId;
+                listItem.dataset.name = department.displayName;
                 listItem.addEventListener("click", function () {
-                    window.location.href = `./department.html?department-id=${this.dataset.id}`;
+                    const departmentName = encodeURIComponent(this.dataset.name);
+                    const departmentId = this.dataset.id;
+                    window.location.href = `./image-list.html?department-name=${departmentName}&department-id=${departmentId}`;
                 });
                 departmentList.appendChild(listItem);
             });
